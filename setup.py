cfb069068f2ce8d9207d67ca8fe33af54051aaed
Emily Stumpf <estumpf@pandora.com>
2017-05-25 12:56:48 -0700

    update README, structure as library

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+setup(
+    author='Emily Stumpf',
+    description='Morse code audio/text library',
+    name='pycq',
+    packages=find_packages(),
+    version='0.1',
+    url='https://github.com/superemily/pycq'
+)
